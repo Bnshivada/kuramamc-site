53a196ec3f987d64aa308cc341f4027f27cabdf7
Shivada <mehmeterensariot15@gmail.com>
2026-01-02 20:45:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("Her şey düzgün çalışıyor ✔");
+async function loadComponent(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+loadComponent("navbar", "components/navbar.html");
+loadComponent("footer", "components/footer.html");
